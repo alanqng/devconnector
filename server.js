35fd639e53927221efc6bdef4f39e9796ad363e9
z0041hhp <alan.ng@siemens.com>
2019-06-10 09:35:51 +0200

    Add routes

--- server.js
+++ server.js
@@ -5,7 +5,16 @@ const app = express();
 
 connectDB()
 
+// Init Middleware
+app.use(express.json({extended: false}));
+
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/post', require('./routes/api/post'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 5000;
 
